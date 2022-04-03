be1df76f65ad827f361b2c379c0095a80125afd7
mozhayka <mozhay2000@gmail.com>
2022-04-03 11:08:07 +0300

    MyContainer

---
+++ Container/MyContainer/IContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Container.MyContainer
+{
+    interface IContainer
+    {
+        public void Register<Interface, Realization>();
+        public Type GetInstance<Interface>();
+    }
+}
