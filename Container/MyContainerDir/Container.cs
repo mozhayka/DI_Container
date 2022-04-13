2a01077613439533afbdac67a49d6e7d8fd3e311
mozhayka <mozhay2000@gmail.com>
2022-04-13 18:22:18 +0300

    add Singleton

--- Container/MyContainerDir/Container.cs
+++ Container/MyContainerDir/Container.cs
@@ -10,20 +10,29 @@ namespace Container
     public class Container : IContainer
     {
         readonly Dictionary<Type, Type> container;
+        readonly Dictionary<Type, object> SingletonObjects;
 
         public Container()
         {
             container = new();
+            SingletonObjects = new();
         }
 
-        public Interface GetInstance<Interface>() 
+        public Interface GetInstance<Interface>()
             where Interface : class
         {
-            if (!container.ContainsKey(typeof(Interface)))
-                throw new Exception("Interface is not registered");
+            if (container.ContainsKey(typeof(Interface)))
+            {
+                var constructor = container[typeof(Interface)].GetConstructor(new Type[] { });
+                return (Interface)constructor.Invoke(new object[] { });
+            }
 
-            var constructor = container[typeof(Interface)].GetConstructor(new Type[] { });
-            return (Interface) constructor.Invoke(new object[] { });
+            if (SingletonObjects.ContainsKey(typeof(Interface)))
+            {
+                return (Interface) SingletonObjects[typeof(Interface)];
+            }
+
+            throw new Exception("Interface is not registered");
         }
 
         public void Register<Interface, Realization>(Lifestyle lifestyle = Lifestyle.Transient)
@@ -33,6 +42,11 @@ namespace Container
             if (lifestyle == Lifestyle.Transient)
             {
                 container[typeof(Interface)] = typeof(Realization);
+            }
+            if (lifestyle == Lifestyle.Singleton)
+            {
+                var constructor = typeof(Realization).GetConstructor(new Type[] { });
+                SingletonObjects[typeof(Interface)] = constructor.Invoke(new object[] { });
             }
         }
     }
