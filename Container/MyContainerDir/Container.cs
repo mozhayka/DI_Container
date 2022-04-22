fcd6f08e65abe3f1085f791c07136cd9de4a634e
mozhayka <mozhay2000@gmail.com>
2022-04-22 12:35:05 +0300

    InstanceProducer

--- Container/MyContainerDir/Container.cs
+++ Container/MyContainerDir/Container.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Container.MyContainerDir;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,90 +11,36 @@ namespace Container
     public class Container : IContainer
     {
         Dictionary<Type, Type> container;
-        readonly Dictionary<Type, System.Reflection.ConstructorInfo> TransientObjects;
+        readonly Dictionary<Type, System.Reflection.ConstructorInfo> TransientConstructors;
         readonly Dictionary<Type, object> SingletonObjects;
+
+        readonly Dictionary<Type, System.Reflection.ConstructorInfo> ScopedConstructors;
         readonly Dictionary<Type, Dictionary<string, object>> ScopedObjects;
+        readonly Dictionary<Type, InstanceProducer> instances;
 
         public Container()
         {
             container = new();
-            TransientObjects = new();
+            TransientConstructors = new();
             SingletonObjects = new();
             ScopedObjects = new();
-
+            instances = new();
         }
 
         public Interface GetInstance<Interface>()
             where Interface : class
         {
-            if (TransientObjects.ContainsKey(typeof(Interface)))
-            {
-                var constructor = TransientObjects[typeof(Interface)];
-                return (Interface)constructor.Invoke(new object[] { });
-            }
+            if (!instances.ContainsKey(typeof(Interface)))
+                throw new Exception("Interface is not registered");
 
-            if (SingletonObjects.ContainsKey(typeof(Interface)))
-            {
-                return (Interface)SingletonObjects[typeof(Interface)];
-            }
-
-            if (ScopedObjects.ContainsKey(typeof(Interface)))
-            {
-                var env = Environment.CurrentDirectory;
-                if (!ScopedObjects[typeof(Interface)].ContainsKey(env))
-                {
-                    var constructor = container[typeof(Interface)].GetConstructor(new Type[] { });
-                    ScopedObjects[typeof(Interface)][env] = constructor.Invoke(new object[] { });
-                }
-                return (Interface)ScopedObjects[typeof(Interface)][env];
-            }
-            throw new Exception("Interface is not registered");
+            return (Interface) instances[typeof(Interface)].GetInstance();
         }
 
         public void Register<Interface, Realization>(Lifestyle lifestyle = Lifestyle.Transient)
             where Interface : class
             where Realization : class, Interface
         {
-            switch (lifestyle)
-            {
-                case Lifestyle.Transient:
-                    RegisterTransient<Interface, Realization>();
-                    break;
-                case Lifestyle.Singleton:
-                    RegisterSingleton<Interface, Realization>();
-                    break;
-                case Lifestyle.Scoped:
-                    RegisterScoped<Interface, Realization>();
-                    break;
-            }
-        }
-
-        public void RegisterTransient<Interface, Realization>()
-            where Interface : class
-            where Realization : class, Interface
-        {
-            container[typeof(Interface)] = typeof(Realization);
-            var constructor = typeof(Realization).GetConstructor(new Type[] { });
-            TransientObjects[typeof(Interface)] = constructor;
-        }
-
-        public void RegisterSingleton<Interface, Realization>()
-            where Interface : class
-            where Realization : class, Interface
-        {
-            container[typeof(Interface)] = typeof(Realization);
-            var constructor = typeof(Realization).GetConstructor(new Type[] { });
-            SingletonObjects[typeof(Interface)] = constructor.Invoke(new object[] { });
-        }
-
-        public void RegisterScoped<Interface, Realization>()
-            where Interface : class
-            where Realization : class, Interface
-        {
-            container[typeof(Interface)] = typeof(Realization);
-            var constructor = typeof(Realization).GetConstructor(new Type[] { });
-            //SingletonObjects[typeof(Interface)] = constructor.Invoke(new object[] { });
-            ScopedObjects[typeof(Interface)] = new();
+            instances[typeof(Interface)] = new(typeof(Interface), typeof(Realization), lifestyle);
         }
     }
 }
---
+++ Container/MyContainerDir/InstanceProducer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Container.IContainer;
+
+namespace Container.MyContainerDir
+{
+    class InstanceProducer
+    {
+        Type IType, RType;
+        Lifestyle lifestyle;
+        Func<object> instanceCreator;
+
+        public InstanceProducer(Type interface_type, Type realization_type, Lifestyle lifestyle)
+        {
+            IType = interface_type;
+            RType = realization_type;
+            this.lifestyle = lifestyle;
+            InitInstanceCreator();
+        }
+
+        public object GetInstance()
+        {
+            return instanceCreator();
+        }
+
+        private void InitInstanceCreator()
+        {
+            switch (lifestyle)
+            {
+                case Lifestyle.Transient:
+                    instanceCreator = () => RType.GetConstructor(Array.Empty<Type>()).Invoke(Array.Empty<object>());
+                    break;
+                case Lifestyle.Singleton:
+                    var SingleObject = RType.GetConstructor(Array.Empty<Type>()).Invoke(Array.Empty<object>());
+                    instanceCreator = () => SingleObject;
+                    break;
+            }
+        }
+    }
+}
