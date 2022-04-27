2414845d7f8f298faec1645bc2e8bbf43d991307
mozhayka <mozhay2000@gmail.com>
2022-04-27 18:17:57 +0300

    Исправил

--- Container/Scope.cs
+++ Container/Scope.cs
@@ -6,11 +6,30 @@ using System.Threading.Tasks;
 
 namespace Container.MyContainerDir
 {
-    class Scope
+    static class Scope
     {
+        static StringBuilder scope = new("scope_0");
+        static int n = 1;
         public static string GetScope()
         {
-            return Environment.CurrentDirectory;
+            return scope.ToString();
+        }
+
+        public static void OpenNewScope()
+        {
+            n++;
+            scope.Append($"_{n}");
+        }
+
+        public static void CloseScope()
+        {
+            var str = scope.ToString();
+            int len = str.Length, i = 1;
+            while (str[len - i] != '_')
+            {
+                i++;
+            }
+            scope.Remove(len - i, i);
         }
     }
 }
--- SimpleInjectorExamples/Program.cs
+++ SimpleInjectorExamples/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using SimpleInjector;
 using SimpleInjectorExamples;
 using TestClasses;
